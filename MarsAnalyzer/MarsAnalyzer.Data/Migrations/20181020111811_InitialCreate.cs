1230a4d807f495a0e3992f38f4e3f700edc6b2e1
Alexander Demianenko <ale.de635@gmail.com>
2018-10-20 16:42:57 +0300

    configured api.

---
+++ MarsAnalyzer/MarsAnalyzer.Data/Migrations/20181020111811_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MarsAnalyzer.Data.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MapPoints",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    X = table.Column<int>(nullable: false),
+                    Y = table.Column<int>(nullable: false),
+                    HeightCoef = table.Column<double>(nullable: false),
+                    TemperatureCoef = table.Column<double>(nullable: false),
+                    GravityCoef = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MapPoints", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MapPoints");
+        }
+    }
+}
--- MarsAnalyzer/MarsAnalyzer.Data/Model/PixelCondition.cs
+++ MarsAnalyzer/MarsAnalyzer.Data/Model/PixelCondition.cs
@@ -2,6 +2,8 @@
 {
     public class PixelCondition
     {
+        public int Id { get; set; }
+
         public int X { get; set; }
         public int Y { get; set; }
 
